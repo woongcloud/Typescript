d62d1f2854055ff30ccebb544bb411dcb35c75ea
sunwoongJang <tjsdnd3103@naver.com>
2023-06-23 15:52:54 +0900

    typescript

---
+++ first.ts
@@ -0,0 +1,11 @@
+const a: string = '5';
+const b: number = 5;
+const c: boolean = true;
+const d: undefined = undefined;
+const e: null = null;
+const arr: string[] = ['123', '456']
+
+
+//function add(x: number, y: number): number { return x + y }
+//const add: (x: number, y: number) => number = (x, y) => x + y;
+//const obj: { lat: number, lon: number } = { lat: 37.5, lon: 127.5 }
